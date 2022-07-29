b4510989be01140b8acc24a565aa084a89004768
Breno Lira <lira_breno@hotmail.com>
2022-07-29 20:26:10 -0300

    chore[css]: edit && delete call to action colors

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "15032ff2e4509b88"; }
+/******/ 		__webpack_require__.h = function() { return "418d65aeb5b310f5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
