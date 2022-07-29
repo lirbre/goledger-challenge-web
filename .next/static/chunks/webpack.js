b0baaa576df9bf36db22cdd50f99eb69269c133b
Breno Lira <lira_breno@hotmail.com>
2022-07-29 12:44:12 -0300

    chore[navbar]: create GoLedger navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2c17b67875d1efb1"; }
+/******/ 		__webpack_require__.h = function() { return "97e1acb44c4cb3d6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,25 @@
-export const Navbar = () => {
-    return (
-        <header className="width-full h-[62.48px] sticky top-0 z-10 border-b-[#e2e8f0] border-b">
+import Image from "next/image";
 
-        </header>
-    )
-}
+export const Navbar = () => {
+  return (
+    <header className="width-full h-[62.48px] sticky top-0 z-10 border-b-[#e2e8f0] border-b">
+      <div className="flex items-center justify-between mx-auto max-w-[1134px] h-full py-2 px-3">
+        <Image
+          src={"/assets/images/goledger-logo.png"}
+          width={188}
+          height={45.48}
+        />
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/lirbre/" target="_blank" rel="noreferrer">
+              {" "}
+              <small className="text-[#4a5568] hover:text-[#4299e1] font-black p-3 tracking-wider transition-all duration-[.25s] curosr-pointer">
+                Contato
+              </small>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
